903539a8adbfbbf71ae945a1c1f65f569489403a
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2017-11-28 14:07:35 +0100

    Dockerfile & own customizations

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $update = $Config->checkUpdate();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1" /> 
-    <title>eZ Server Monitor - <?php echo Misc::getHostname(); ?></title>
+    <title>Server Monitor</title>
     <link rel="stylesheet" href="web/css/utilities.css" type="text/css">
     <link rel="stylesheet" href="web/css/frontend.css" type="text/css">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
@@ -71,51 +71,8 @@ $update = $Config->checkUpdate();
 
 <div id="main-container">
 
-    <div class="box column-left" id="esm-system">
-        <div class="box-header">
-            <h1>System</h1>
-            <ul>
-                <li><a href="#" class="reload" onclick="esm.reloadBlock('system');"><span class="icon-cycle"></span></a></li>
-            </ul>
-        </div>
 
-        <div class="box-content">
-            <table class="firstBold">
-                <tbody>
-                    <tr>
-                        <td>Hostname</td>
-                        <td id="system-hostname"></td>
-                    </tr>
-                    <tr>
-                        <td>OS</td>
-                        <td id="system-os"></td>
-                    </tr>
-                    <tr>
-                        <td>Kernel version</td>
-                        <td id="system-kernel"></td>
-                    </tr>
-                    <tr>
-                        <td>Uptime</td>
-                        <td id="system-uptime"></td>
-                    </tr>
-                    <tr>
-                        <td>Last boot</td>
-                        <td id="system-last_boot"></td>
-                    </tr>
-                    <tr>
-                        <td>Current user(s)</td>
-                        <td id="system-current_users"></td>
-                    </tr>
-                    <tr>
-                        <td>Server date & time</td>
-                        <td id="system-server_date"></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-    <div class="box column-right" id="esm-load_average">
+    <div class="box" id="esm-load_average">
         <div class="box-header">
             <h1>Load Average</h1>
             <ul>
@@ -138,75 +95,6 @@ $update = $Config->checkUpdate();
                 <h3>5 min</h3>
                 <input type="text" class="gauge" id="load-average_5" value="0" data-height="100" data-width="150" data-min="0" data-max="100" data-readOnly="true" data-fgColor="#BED7EB" data-angleOffset="-90" data-angleArc="180">
             </div>
-        </div>
-    </div>
-
-
-
-    <div class="box column-right" id="esm-cpu">
-        <div class="box-header">
-            <h1>CPU</h1>
-            <ul>
-                <li><a href="#" class="reload" onclick="esm.reloadBlock('cpu');"><span class="icon-cycle"></span></a></li>
-            </ul>
-        </div>
-
-        <div class="box-content">
-            <table class="firstBold">
-                <tbody>
-                    <tr>
-                        <td>Model</td>
-                        <td id="cpu-model"></td>
-                    </tr>
-                    <tr>
-                        <td>Cores</td>
-                        <td id="cpu-num_cores"></td>
-                    </tr>
-                    <tr>
-                        <td>Speed</td>
-                        <td id="cpu-frequency"></td>
-                    </tr>
-                    <tr>
-                        <td>Cache</td>
-                        <td id="cpu-cache"></td>
-                    </tr>
-                    <tr>
-                        <td>Bogomips</td>
-                        <td id="cpu-bogomips"></td>
-                    </tr>
-                    <?php if ($Config->get('cpu:enable_temperature')): ?>
-                        <tr>
-                            <td>Temperature</td>
-                            <td id="cpu-temp"></td>
-                        </tr>
-                    <?php endif; ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-
-
-    <div class="box column-left" id="esm-network">
-        <div class="box-header">
-            <h1>Network usage</h1>
-            <ul>
-                <li><a href="#" class="reload" onclick="esm.reloadBlock('network');"><span class="icon-cycle"></span></a></li>
-            </ul>
-        </div>
-
-        <div class="box-content">
-            <table>
-                <thead>
-                    <tr>
-                        <th class="w15p">Interface</th>
-                        <th class="w20p">IP</th>
-                        <th>Receive</th>
-                        <th>Transmit</th>
-                    </tr>
-                </thead>
-                <tbody></tbody>
-            </table>
         </div>
     </div>
 
@@ -310,69 +198,6 @@ $update = $Config->checkUpdate();
             </table>
         </div>
     </div>
-
-
-    <div class="cls"></div>
-
-
-    <div class="t-center">
-        <div class="box column-left column-33" id="esm-last_login">
-            <div class="box-header">
-                <h1>Last login</h1>
-                <ul>
-                    <li><a href="#" class="reload" onclick="esm.reloadBlock('last_login');"><span class="icon-cycle"></span></a></li>
-                </ul>
-            </div>
-
-            <div class="box-content">
-                <?php if ($Config->get('last_login:enable') == true): ?>
-                    <table>
-                        <tbody></tbody>
-                    </table>
-                <?php else: ?>
-                    <p>Disabled</p>
-                <?php endif; ?>
-            </div>
-        </div>
-
-
-
-        <div class="box column-right column-33" id="esm-services">
-            <div class="box-header">
-                <h1>Services status</h1>
-                <ul>
-                    <li><a href="#" class="reload" onclick="esm.reloadBlock('services');"><span class="icon-cycle"></span></a></li>
-                </ul>
-            </div>
-
-            <div class="box-content">
-                <table>
-                    <tbody></tbody>
-                </table>
-            </div>
-        </div>
-
-
-
-
-        <div class="box t-center" style="margin: 0 33%;" id="esm-ping">
-            <div class="box-header">
-                <h1>Ping</h1>
-                <ul>
-                    <li><a href="#" class="reload" onclick="esm.reloadBlock('ping');"><span class="icon-cycle"></span></a></li>
-                </ul>
-            </div>
-
-            <div class="box-content">
-                <table>
-                    <tbody></tbody>
-                </table>
-            </div>
-        </div>
-
-    </div>
-
-    
 
     <div class="cls"></div>
 
